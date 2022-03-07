e54a6137f3e7523bc57913e39eccf81e5153b21b
Kleverson Kenji <kleversonkenji@gmail.com>
2022-03-07 12:15:28 -0300

    listening new questions

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import logoImg from "../assets/images/logo.svg";
@@ -9,6 +9,30 @@ import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
 
 import "../styles/room.scss";
+
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      author: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+  }
+>;
+
+type Question = {
+  id: string;
+  author: {
+    author: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+};
 
 type RoomParams = {
   id: string;
@@ -23,8 +47,43 @@ export function Room() {
   // state das questões
   const [newQuestion, setNewQuestion] = useState("");
 
+  // state de array vazio
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  // state vazio
+  const [title, setTitle] = useState("");
+
   // declaração mais transparente
   const roomId = params.id;
+
+  // observar sala e atualizar dados quando mudados
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    // once -> ouve evento uma vez / on -> ouve evento quando mudar
+    roomRef.once("value", (room) => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+          };
+        }
+      );
+
+      // muda state do título
+      setTitle(databaseRoom.title);
+
+      // muda state da questão
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId]);
 
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault();
@@ -67,8 +126,8 @@ export function Room() {
 
       <main>
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas</span>
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
         <form onSubmit={handleSendQuestion}>
           <textarea
@@ -92,6 +151,8 @@ export function Room() {
             </Button>
           </div>
         </form>
+
+        {JSON.stringify(questions)}
       </main>
     </div>
   );
